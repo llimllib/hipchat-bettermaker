244c3f543dd7cc35ba998d0a59f2ab2b438b094c
bill <bill.mill@gmail.com>
2012-04-18 12:42:45 -0400

    rearrange code a bit, remove doubling of emoticon size

--- js/hipchat.js
+++ js/hipchat.js
@@ -1,23 +1,31 @@
 // append text to the message input field
 function append(text) {
-    var t = $("#message_input").val;
-    t(t() + text);
+    var t = $("#message_input");
+    t.val(t.val() + text);
+}
+
+function show_emoticons() {
+}
+
+function hide_emoticons() {
+}
+
+function create_emoticons() {
 }
 
 function hook_chat() {
+    create_emoticons();
+    
     /* hook the chat.scroll_chat function to update stuff every time we need to scroll */
     old_scroll_chat = chat.scroll_chat;
     chat.scroll_chat = function() {
         console.log("scroll hook called");
 
+        /* add title attributes to emoticons */
         $("img[name='emoticon']:not([title])").each(function(i,elt) {
             elt=$(elt);
             /* add a title attribute to emoticons */
             elt.attr("title", elt.attr("alt"));
-
-            /* double size emoticons */
-            elt.attr("height", "50");
-            elt.attr("width", "40");
         });
 
         /* double the max size of images not already doubled */
@@ -26,19 +34,6 @@ function hook_chat() {
             elt.css("max-height", "500px");
             elt.css("max-width", "600px");
         });
-
-        var emoticons = document.createElement("div");
-        emoticons.id = "emoticons";
-        $(emoticons).css("display:none");
-        (document.head || document.body || document.documentElement).appendChild(emoticons);
-
-        $(emoticons).html("<a href='#' onclick='append(\"(awyeah)\")'><img src=\"http://hipchat.com/img/emoticons/awyea.png\"></a>"); 
-
-        if ($("#emoticon-toggle").length == 0) {
-
-            var emoticon_toggle = '<td width="1%" id="emoticon-toggle"><a href="#"><img src="http://hipchat.com/img/emoticons/awyea.png"></a></td>';
-            $("#send_message_button").parent().before(emoticon_toggle);
-        }
 
         old_scroll_chat.apply(chat, arguments);
     }
